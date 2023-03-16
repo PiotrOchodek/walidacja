5771d65464ecf71b89c572f331b35ae08595e5b3
Piotr Ochodek <ohis@op.pl>
2023-03-16 09:49:03 +0100

    Dopisanie dwóch funkcji robiących to samo ale jedna bez tworzenia stałej, druga w zapisie strzałkowym

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function validatePassword(password) {
   const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
+  // [0-9] może być zamienione na \d - wyszukuje cyfry
   if (typeof password == typeof '') {
     return characters.test(password);
   } else {
@@ -9,3 +10,30 @@ function validatePassword(password) {
 
 const word = validatePassword('dom1@');
 console.log(word);
+
+// inny zapis tej samej funkcji bez tworzenia stałej
+function validatePassword2(password) {
+  if (typeof password == typeof '') {
+    return /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/.test(password);
+  } else {
+    console.log('Wrong input');
+  }
+}
+
+const word2 = validatePassword2('dom@');
+console.log(word2);
+
+//zapis w formie strzałkowej funkcji 
+
+const validatePassword3 = password => {
+  const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
+  // [0-9] może być zamienione na \d - wyszukuje cyfry
+  if (typeof password == typeof '') {
+    return characters.test(password);
+  } else {
+    console.log('Wrong input');
+  }
+}
+
+const word3 = validatePassword3('!123456789');
+console.log(word3);
