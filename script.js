955f2298b23fb518e1102daa5aa5a39c13ea61ff
Piotr Ochodek <ohis@op.pl>
2023-03-17 11:02:13 +0100

    add function validatePassword4 that checks conditions one by one: string type, number of characters, digits and special characters.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+const validatePassword4 = (password) => {
+  const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
+
+  if (typeof password != typeof 'string') {
+    console.log('Wrong input! Enter string type parameter');
+    return;
+  }
+  if (password.length > 10 || password.length < 3) {
+    console.log('Wrong number of characters');
+    return;
+  } if (!characters.test(password)) {
+    console.log('Must contain at least one digit one of the special characters "!","@","#"')
+    return
+  } else {
+    console.log('Correct password')
+  }
+};
+
+const word4 = validatePassword4('ab1#');
+console.log(word4);
+
+//------------------------------------------------------------
+
 function validatePassword(password) {
   const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
   // [0-9] może być zamienione na \d - wyszukuje cyfry
-  if (typeof password == typeof '') {
+  if (typeof password == typeof 'string') {
     return characters.test(password);
   } else {
     console.log('Wrong input');
+    return;
   }
 }
 
@@ -13,27 +37,29 @@ console.log(word);
 
 // inny zapis tej samej funkcji bez tworzenia stałej
 function validatePassword2(password) {
-  if (typeof password == typeof '') {
+  if (typeof password == typeof 'string') {
     return /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/.test(password);
   } else {
     console.log('Wrong input');
+    return;
   }
 }
 
 const word2 = validatePassword2('dom@');
 console.log(word2);
 
-//zapis w formie strzałkowej funkcji 
+//zapis w formie strzałkowej funkcji
 
-const validatePassword3 = password => {
+const validatePassword3 = (password) => {
   const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
   // [0-9] może być zamienione na \d - wyszukuje cyfry
-  if (typeof password == typeof '') {
+  if (typeof password == typeof 'string') {
     return characters.test(password);
   } else {
     console.log('Wrong input');
+    return;
   }
-}
+};
 
 const word3 = validatePassword3('!123456789');
 console.log(word3);
