af8d44b07a59f98e0990d91c58f9cd5915541527
Piotr Ochodek <ohis@op.pl>
2023-03-17 12:52:23 +0100

    Function return true or false

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const validatePassword4 = (password) => {
 
   if (typeof password !== 'string') {
     console.log('Wrong input! Enter string type parameter');
-    return;
+    return false;
   }
   if (password.length > 10 || password.length < 3) {
     console.log('Wrong number of characters');
-    return;
+    return false;
   }
   if (
     !(
@@ -19,58 +19,60 @@ const validatePassword4 = (password) => {
     console.log(
       'Must contain at least one of the special characters "!","@","#"'
     );
+    return false;
   }
   if (!characters.test(password)) {
     console.log('Must contain at least one digit');
-    return;
+    return false;
   }
   console.log('Correct password');
+  return true;
 };
 
-const word4 = validatePassword4('dom!1');
+const word4 = validatePassword4('asd@2');
 console.log(word4);
 
 //------------------------------------------------------------
 
-function validatePassword(password) {
-  const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
-  // [0-9] może być zamienione na \d - wyszukuje cyfry
-  if (typeof password === 'string') {
-    return characters.test(password);
-  } else {
-    console.log('Wrong input');
-    return;
-  }
-}
+// function validatePassword(password) {
+//   const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
+//   // [0-9] może być zamienione na \d - wyszukuje cyfry
+//   if (typeof password === 'string') {
+//     return characters.test(password);
+//   } else {
+//     console.log('Wrong input');
+//     return;
+//   }
+// }
 
-const word = validatePassword('dom1@');
-console.log(word);
+// const word = validatePassword('dom1@');
+// console.log(word);
 
-// inny zapis tej samej funkcji bez tworzenia stałej
-function validatePassword2(password) {
-  if (typeof password === 'string') {
-    return /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/.test(password);
-  } else {
-    console.log('Wrong input');
-    return;
-  }
-}
+// // inny zapis tej samej funkcji bez tworzenia stałej
+// function validatePassword2(password) {
+//   if (typeof password === 'string') {
+//     return /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/.test(password);
+//   } else {
+//     console.log('Wrong input');
+//     return;
+//   }
+// }
 
-const word2 = validatePassword2('dom@');
-console.log(word2);
+// const word2 = validatePassword2('dom@');
+// console.log(word2);
 
-//zapis w formie strzałkowej funkcji
+// //zapis w formie strzałkowej funkcji
 
-const validatePassword3 = (password) => {
-  const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
-  // [0-9] może być zamienione na \d - wyszukuje cyfry
-  if (typeof password === 'string') {
-    return characters.test(password);
-  } else {
-    console.log('Wrong input');
-    return;
-  }
-};
+// const validatePassword3 = (password) => {
+//   const characters = /^(?=.*[!@#])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{3,10}$/;
+//   // [0-9] może być zamienione na \d - wyszukuje cyfry
+//   if (typeof password === 'string') {
+//     return characters.test(password);
+//   } else {
+//     console.log('Wrong input');
+//     return;
+//   }
+// };
 
-const word3 = validatePassword3('!123456789');
-console.log(word3);
+// const word3 = validatePassword3('!123456789');
+// console.log(word3);
